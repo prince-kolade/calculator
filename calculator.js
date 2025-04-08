11d67ff5bc46ffb759a0dcc83dda9be61be1c819
unknown <princederrick100@gmail.com>
2025-04-08 11:31:07 +0100

    first commit

--- calculator.js
+++ calculator.js
@@ -19,11 +19,24 @@ function clearResult() {
 
 
 
+ function deleteValue(){
+    display.value = display.value.slice(0, -1);
+ }
 
 
-
-
-
+function moonClick(){
+    document.getElementById("calculator").style.backgroundColor = "black"
+    document.getElementById("kolade").style.color = "white"
+    document.getElementById("sun").style.color = "white"
+    document.getElementById("kolade").style.color = "white"
+    document.getElementById("moon").style.color = "gray"
+}
+function sunClick(){
+    document.getElementById("calculator").style.backgroundColor = "white"
+    document.getElementById("kolade").style.color = "black"
+    document.getElementById("moon").style.color = "black"
+    document.getElementById("sun").style.color = "gray"
+}
 
 function numberValue(value){
     console.log(value)
